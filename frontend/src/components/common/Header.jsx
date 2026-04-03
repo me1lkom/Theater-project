9e8891cb8bd39c9ffd174b3c935a8002cd029070
me1lkom <melkom06@gmail.com>
2026-04-03 13:04:07 +0300

    кликабельное лого

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate(`/`);
+  };
+
   return (
     <header>
-      <div>Логотип</div>
+      <div onClick={handleLogoClick}>Логотип</div>
       <nav>Меню</nav>
     </header>
   );
