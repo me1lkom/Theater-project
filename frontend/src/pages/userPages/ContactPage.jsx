34a269990a8e27ce900b0dfec373def63cb11bd9
me1lkom <melkom06@gmail.com>
2026-05-19 17:15:13 +0300

    Виджет яндека карт на странице контактов

--- frontend/src/pages/userPages/ContactPage.jsx
+++ frontend/src/pages/userPages/ContactPage.jsx
@@ -10,6 +10,10 @@ export default function ContactPage() {
                 <p>Телефон: +7 (905) 789-12-12</p>
                 <p>Email: emailtheater@gmail.com</p>
                 <p>Часы работы: Пн–Вс: 10:00 – 20:00</p>
+
+            </div>
+            <div className={styles.mapContainer}>
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aed8f0526508bd7e65739988419883a7f59617ebbd38f7061a307023f907ffc94&amp;source=constructor" frameborder="0"></iframe>
             </div>
         </div>
     )
