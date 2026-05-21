0b6ee48681a226d4d6bf782310bfb9d389466035
Rozjog <muratilya@mail.ru>
2026-05-22 01:28:23 +0300

    Исправление метода возврата

--- backend/api/payment.py
+++ backend/api/payment.py
@@ -1,10 +1,11 @@
-from yookassa import Configuration, Payment
+from yookassa import Configuration, Payment, Refund
 import uuid
 from django.conf import settings
-from .models import TicketStatus
+from api.models import TicketStatus
 
 Configuration.account_id = settings.YOOKASSA_SHOP_ID 
 Configuration.secret_key = settings.YOOKASSA_SECRET_KEY
+
 
 def create_payment(amount, description, return_url):
     idempotence_key = str(uuid.uuid4())
@@ -25,51 +26,44 @@ def create_payment(amount, description, return_url):
         "capture": True
     }, idempotence_key)
     
-    print(f"YooKassa payment created:")
-    print(f"  ID: {payment.id}")
-    print(f"  Return URL: {return_url}")
-    print(f"  Confirmation URL: {payment.confirmation.confirmation_url}")
-    
     return payment.confirmation.confirmation_url, payment.id
+
 
 def check_payment(payment_id):
     try:
         payment = Payment.find_one(payment_id)
-        print(f"payment_id: {payment_id}")
-        print(f"status: {payment.status}")
         return payment.status == "succeeded"
     except Exception as e:
         print(f"Ошибка при проверке платежа: {e}")
         return False
 
+
 def refund_payment(payment_id):
     try:
-        payment = Payment.find_one(payment_id)
-        if payment.status == "succeeded":
-            refund = payment.refund({
-                "amount": {
-                    "value": payment.amount.value,
-                    "currency": payment.amount.currency
-                }
-            })
-            return refund.status == "succeeded"
+        refund = Refund.create({
+            "payment_id": payment_id,
+            "amount": {
+                "value": "100.00",
+                "currency": "RUB"
+            }
+        })
+        return refund.status == "succeeded"
     except Exception as e:
         print(f"Ошибка при возврате: {e}")
-    return False
+        return False
 
 
 def refund_ticket(ticket):
-   # Возврат денег за ОДИН билет
     if not ticket.payment:
         return False, "Билет не связан с платежом"
     
     try:
-        refund = Payment.refund({
+        refund = Refund.create({
+            "payment_id": ticket.payment.payment_id,
             "amount": {
                 "value": str(ticket.price_paid),
                 "currency": "RUB"
-            },
-            "payment_id": ticket.payment.payment_id
+            }
         })
         
         if refund.status == "succeeded":
@@ -77,6 +71,8 @@ def refund_ticket(ticket):
             ticket.status = returned_status
             ticket.save()
             return True, f"Возвращено {ticket.price_paid} руб."
-        return False, "Ошибка возврата"
+        else:
+            return False, "Ошибка возврата"
+            
     except Exception as e:
         return False, str(e)
